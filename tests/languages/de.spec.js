fdcd3c9383c57a457fd8b15462bfb2e60e287ca9
DanielHabenicht <daniel-habenicht@outlook.de>
2023-06-20 22:38:08 +0000

    fix test title

--- tests/languages/de.spec.js
+++ tests/languages/de.spec.js
@@ -22,12 +22,10 @@ test.use({
   timezoneId: "America/New_York",
 });
 
-test("test german text display", async ({ page }) => {
+test("test german text display: forced", async ({ page }) => {
   await page.goto(
     "/?entityID=https://example.org&returnIDParam=providerUri&return=https%3a%2f%2fexample.org%2fShibboleth.sso%2fLogin%3freturnUrl%3dhttp%253a%252f%252fexample.org"
   );
-  
-
 
   // Expect a embedded discovery service to render
   await expect(
