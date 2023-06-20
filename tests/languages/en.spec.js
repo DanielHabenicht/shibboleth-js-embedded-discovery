0c861306783e755e02142e61e35f704303803109
DanielHabenicht <daniel-habenicht@outlook.de>
2023-06-20 22:32:44 +0000

    fix spec title

--- tests/languages/en.spec.js
+++ tests/languages/en.spec.js
@@ -6,7 +6,7 @@ test.use({
   timezoneId: "America/New_York",
 });
 
-test("test german text display", async ({ page }) => {
+test("test english text display", async ({ page }) => {
   await page.goto(
     "/?entityID=https://example.org&returnIDParam=providerUri&return=https%3a%2f%2fexample.org%2fShibboleth.sso%2fLogin%3freturnUrl%3dhttp%253a%252f%252fexample.org"
   );
